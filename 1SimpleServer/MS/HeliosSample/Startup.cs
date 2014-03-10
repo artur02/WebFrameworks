b74301a071f83dc714088087ed9dbfb094ff1d04
Artur Herczeg <Artur_Herczeg1@epam.com>
2014-03-10 14:27:34 +0100

    ASP.NET Helios

---
+++ 1SimpleServer/MS/HeliosSample/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System;
+using Owin;
+
+namespace HeliosSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            // Requires Windows 8 or Windows Server 2012
+            app.Run(async context =>
+            {
+                context.Response.ContentType = "text/plain";
+
+                await context.Response.WriteAsync("Assemblies in AppDomain:" + Environment.NewLine);
+                foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    await context.Response.WriteAsync(asm.FullName + Environment.NewLine);
+                }
+                await context.Response.WriteAsync(Environment.NewLine);
+
+                await context.Response.WriteAsync("Stack trace:" + Environment.NewLine);
+                await context.Response.WriteAsync(Environment.StackTrace);
+            });
+        }
+    }
+}
