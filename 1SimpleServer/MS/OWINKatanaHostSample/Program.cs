fcdec21f54b729ec5aa22cfd6899696038d03e68
Artur Herczeg <Artur_Herczeg1@epam.com>
2014-03-10 14:37:26 +0100

    OWIN Katana Sample with Self Host

---
+++ 1SimpleServer/MS/OWINKatanaHostSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OWINKatanaHostSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<OWINKatanaSample.Startup>("http://localhost:9000"))
+            {
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 1SimpleServer/MS/OWINKatanaSample/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Owin;
+
+namespace OWINKatanaSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+
+            app.Run(context =>
+            {
+                if (context.Request.Path.Value == "/fail")
+                {
+                    throw new Exception("Random exception");
+                }
+
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+        }
+    }
+}
